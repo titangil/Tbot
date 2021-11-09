2964664aa783610f1c2fddd5b2c049abb5f87731
titan <janthorititan@gmail.com>
2021-11-09 19:23:53 +0700

    a

--- app.py
+++ app.py
@@ -65,10 +65,10 @@ def event_handle(event):
         translation = translator.translate(msg)
         if translation.src == 'en':
             translation = translator.translate(msg, dest='ja')
-            replyObj = TextSendMessage(text="翻訳  🇺🇸 => 🇯🇵 :　\n\n"+"　　　 「"+translation.text+"」")
+            replyObj = TextSendMessage(text="翻訳  🇺🇸 => 🇯🇵 :　\n\n"+"　「"+translation.text+"」")
         elif translation.src == 'ja':
             translation = translator.translate(msg, dest='en')
-            replyObj = TextSendMessage(text="Translation  🇯🇵 => 🇺🇸 : \n\n"+" 　　　""+translation.text+""")
+            replyObj = TextSendMessage(text="Translation  🇯🇵 => 🇺🇸 : \n\n"+" '"+translation.text+"'")
         
         line_bot_api.reply_message(rtoken, replyObj)
 
