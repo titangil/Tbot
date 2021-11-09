f9ff20e4e7b749415dc3817068ffa89fd6f9e3b2
Titan <nothingtodohere007@gmail.com>
2021-11-10 01:54:24 +0700

    make it better

--- app.py
+++ app.py
@@ -79,9 +79,9 @@ def event_handle(event):
         wordslength = len(words.words)
         for x in range(wordslength):
             if len(words.words[x]) == 1:
-                wordx = wordx + words.words[x] + "           "+ words.postags[x]+"\n"
+                wordx = wordx + words.words[x] + "          "+ words.postags[x]+"\n"
             elif len(words.words[x]) == 2:
-                wordx = wordx + words.words[x] + "         "+ words.postags[x]+"\n"
+                wordx = wordx + words.words[x] + "        "+ words.postags[x]+"\n"
             elif len(words.words[x]) == 3:
                 wordx = wordx + words.words[x] + "  "+ words.postags[x]+"\n"
         if translation.src == 'en':
